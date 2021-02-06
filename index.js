babaeaf74f89016123a0cbadd0c26e82dc3e3bb6
Ken Sumiyoshi <kensumiyoshi1987@gmail.com>
2021-02-06 17:18:07 -0500

    JS added

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict'
+
+{
+    const open = document.getElementById('open');
+    const close = document.getElementById('close');
+    const madal = document.getElementById('modal');
+    const mask = document.getElementById('mask');
+
+    open.addEventListener('click',() => {
+        modal.classList.remove('hidden');
+        mask.classList.remove('hidden');
+    });
+
+    close.addEventListener('click',() => {
+        modal.classList.add('hidden');
+        mask.classList.add('hidden');
+    });
+
+    mask.addEventListener('click',() => {
+        // modal.classList.add('hidden');
+        // mask.classList.add('hidden');
+        close.click();
+    });
+
+
+    const open2 = document.getElementById('open2');
+    const close2 = document.getElementById('close2');
+    const madal2 = document.getElementById('modal2');
+    const mask2 = document.getElementById('mask2');
+
+    open2.addEventListener('click',() => {
+        modal2.classList.remove('hidden2');
+        mask2.classList.remove('hidden2');
+    });
+
+    close2.addEventListener('click',() => {
+        modal2.classList.add('hidden2');
+        mask2.classList.add('hidden2');
+    });
+
+    mask2.addEventListener('click',() => {
+        // modal.classList.add('hidden');
+        // mask.classList.add('hidden');
+        close2.click();
+    });
+
+    
+}
